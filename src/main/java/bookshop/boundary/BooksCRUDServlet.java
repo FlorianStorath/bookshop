ef5c47ff8476121895a0df37957c6baedb90771c
Florian Storath <florianstorath@Florians-MacBook-Pro.local>
2020-11-10 15:40:40 +0100

    redirect works after Data creation

--- src/main/java/bookshop/boundary/BooksCRUDServlet.java
+++ src/main/java/bookshop/boundary/BooksCRUDServlet.java
@@ -28,7 +28,7 @@ public class BooksCRUDServlet extends HttpServlet {
 		int id = Integer.parseInt(request.getParameter("id"));
 		int year = Integer.parseInt(request.getParameter("year"));
 		BooksData.getInstance().add(new Book(id,title,author,year));
-		//response.sendRedirect("http://localhost:8080/sample-0.0.1-SNAPSHOT/books");
+		response.sendRedirect("books");
 //       try(PrintWriter out = response.getWriter()) {
 //			out.println("Datensatz angelegt!");
 //				};
